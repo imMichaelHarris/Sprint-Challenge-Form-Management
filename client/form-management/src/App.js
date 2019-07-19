bcbb3eee14dabd4bd3316b70a1569ae020ec0e2d
Michael Harris <mhguitar1997@gmail.com>
2019-07-19 09:42:14 -0700

    Import axios WithAuth in App.js

--- client/form-management/src/App.js
+++ client/form-management/src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import useLocalStorage from "./hooks/useLocalStorage";
 import RegisterForm from "./components/RegisterForm";
 import RecipeList from "./views/RecipeList";
+import { axiosWithAuth } from "./utility/axiosWithAuth";
 
 function App() {
   const [token, setToken] = useLocalStorage("token");
 
-
+  useEffect(() => {}, []);
   return (
     <div className="App">
       <RegisterForm setToken={setToken} />
