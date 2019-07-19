315254488d348aecd7938e992632fbe0be12971d
Michael Harris <mhguitar1997@gmail.com>
2019-07-19 09:40:43 -0700

    Import useEffect in App.js

--- client/form-management/src/App.js
+++ client/form-management/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import useLocalStorage from "./hooks/useLocalStorage";
 import RegisterForm from "./components/RegisterForm";
