04dd9cb9dcccb7bc8d3022b32705120b9e7a1cfb
Michael Harris <mhguitar1997@gmail.com>
2019-07-19 11:01:45 -0700

    Private Route HOC write - not tested

--- client/form-management/src/utility/PrivateRoute.js
+++ client/form-management/src/utility/PrivateRoute.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Route, Redirect} from 'react-router-dom'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ component: Component, token, ...rest }) => {
+  return token ? (
+    <Route {...rest} render={props => <Component {...props} />} />
+  ) : (
+    <Redirect to="/" />
+  );
 };
 
 export default PrivateRoute;
