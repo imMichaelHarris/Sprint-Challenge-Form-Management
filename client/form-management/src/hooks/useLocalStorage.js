bdd89e4b7d355913d6306e967f6a1c4bf4b34e7a
Michael Harris <mhguitar1997@gmail.com>
2019-07-19 08:46:55 -0700

    LocalStorage custom hook write

--- client/form-management/src/hooks/useLocalStorage.js
+++ client/form-management/src/hooks/useLocalStorage.js
@@ -1,7 +1,12 @@
 import {useState} from 'react'
 
 const useLocalStorage = (key, initialValue) => {
-    const [localToken, setLocalToken] = useState()
+    const [localToken, setLocalToken] = useState();
+
+    const token = localStorage.getItem(key);
+    token ? setLocalToken(token) : setLocalToken(initialValue)
+
+    return [localToken, setLocalToken]
 }
 
 export default useLocalStorage;
