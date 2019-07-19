aa97af044c7d2c7726e03a34a889e7aa66f45fc3
Michael Harris <mhguitar1997@gmail.com>
2019-07-19 10:02:02 -0700

    List view component will render Recipe component after getting data from server

--- client/form-management/src/views/RecipeList.js
+++ client/form-management/src/views/RecipeList.js
@@ -3,7 +3,7 @@ import { axiosWithAuth } from "../utility/axiosWithAuth";
 import Recipe from "../components/Recipe";
 
 const RecipeList = ({ token }) => {
-  const [recipes, setRecipes] = useState();
+  const [recipes, setRecipes] = useState([]);
   useEffect(() => {
     axiosWithAuth
       .get("/restricted/data", {
@@ -13,12 +13,20 @@ const RecipeList = ({ token }) => {
       })
       .then(res => {
         console.log(res);
+        setRecipes(res.data);
       })
       .catch(err => {
         console.log(err.response);
       });
   }, []);
-  return <div>List</div>;
+  console.log(recipes)
+  return (
+    <div>
+      {recipes.map((recipe, index) => (
+        <Recipe key={index} recipe={recipe} />
+      ))}
+    </div>
+  );
 };
 
 export default RecipeList;
